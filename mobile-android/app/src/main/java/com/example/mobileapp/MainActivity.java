d74a6287f326ae82cc7215a1560369feaa803f81
Natsu638 <65947196+Natsu638@users.noreply.github.com>
2026-01-12 18:37:45 +0100

    Pruebas de maquetado
    Pruebas de maquetado de la pagina principal

--- mobile-android/app/src/main/java/com/example/mobileapp/MainActivity.java
+++ mobile-android/app/src/main/java/com/example/mobileapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.mobileapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+
+    View.OnClickListener loginMaq = new View.OnClickListener(){
+        public void onClick(View v){
+        }
+    };
+    public void buttonInicioSesion(){
+    Button inicioSesion = (Button)findViewById(R.id.iniciarSesion);
     }
 }
