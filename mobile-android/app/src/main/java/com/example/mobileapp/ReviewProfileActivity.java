63c37b40bfe59047d10cbfb6f049db3a350bcd7e
Natsu638 <adriancidgomez@gmail.com>
2026-02-06 13:33:00 +0100

    Review maquetacion

--- mobile-android/app/src/main/java/com/example/mobileapp/ReviewProfileActivity.java
+++ mobile-android/app/src/main/java/com/example/mobileapp/ReviewProfileActivity.java
@@ -9,6 +9,5 @@ public class ReviewProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reviews_profile_activity);
-
     }
 }
