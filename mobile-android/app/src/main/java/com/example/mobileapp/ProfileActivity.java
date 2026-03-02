02f2bcf74a51f9a7c826dbeb85bd4d7c924f3849
Natsu638 <65947196+Natsu638@users.noreply.github.com>
2026-03-02 13:15:50 +0100

    limpieza de codigo

--- mobile-android/app/src/main/java/com/example/mobileapp/ProfileActivity.java
+++ mobile-android/app/src/main/java/com/example/mobileapp/ProfileActivity.java
@@ -2,16 +2,9 @@ package com.example.mobileapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import com.google.android.material.imageview.ShapeableImageView;
-import com.google.android.material.navigation.NavigationView;
 
 public class ProfileActivity extends AppCompatActivity {
 
